701435108e46c2d3abb366119b55f0baf45be4ac
Gloria <gloria.wan.glory@gmail.com>
2023-06-07 20:43:22 -0400

    changed for loop in useAvailable letter to use the 'of' keyword instead of a counter by length

--- src/adagrams.js
+++ src/adagrams.js
@@ -23,11 +23,11 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   let workingLettersInHand = JSON.parse(JSON.stringify(lettersInHand))
   input = input.toUpperCase() 
   
-  for (let i = 0; i < input.length; i++) {
-    if (workingLettersInHand.includes(input[i]) === true) {
-      let letterIndex = workingLettersInHand.indexOf(input[i])
+  for (let letter of input) {
+    if (workingLettersInHand.includes(letter) === true) {
+      let letterIndex = workingLettersInHand.indexOf(letter)
       workingLettersInHand.splice(letterIndex,1)
-    } else if (workingLettersInHand.includes(input[i]) === false) {
+    } else if (workingLettersInHand.includes(letter) === false) {
         return false
     }
   } 
@@ -39,33 +39,13 @@ export const scoreWord = (word) => {
   word = word.toUpperCase()
 
   const letterScore = {
-      A: 1,
-      B: 3,
-      C: 3,
-      D: 2,
-      E: 1,
-      F: 4,
-      G: 2,
-      H: 4,
-      I: 1,
-      J: 8,
-      K: 5,
-      L: 1,
-      M: 3,
-      N: 1,
-      O: 1,
-      P: 3,
-      Q: 10,
-      R: 1,
-      S: 1,
-      T: 1,
-      U: 1,
-      V: 4,
-      W: 4,
-      X: 8,
-      Y: 4,
-      Z: 10,
-      "":0
+      A: 1, B: 3, C: 3, D: 2,
+      E: 1, F: 4, G: 2, H: 4,
+      I: 1, J: 8, K: 5, L: 1,
+      M: 3, N: 1, O: 1, P: 3,
+      Q: 10, R: 1, S: 1, T: 1,
+      U: 1, V: 4, W: 4, X: 8,
+      Y: 4, Z: 10, "":0
     }
 
   for (let letter of word) {
