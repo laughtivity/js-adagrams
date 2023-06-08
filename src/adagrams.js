b1e5db08e4768d22cc94637764e3852824b50e70
Gloria <gloria.wan.glory@gmail.com>
2023-06-07 20:33:09 -0400

    removed comments and print statements

--- src/adagrams.js
+++ src/adagrams.js
@@ -7,38 +7,26 @@ const letterPool = ['A','A','A','A','A','A','A','A','A','B','B','C','C','D',
 'X','Y','Y','Z']
 
 export const drawLetters = () => {
-  // Implement this method for wave 1
   let workingLetterPool = JSON.parse(JSON.stringify(letterPool))
   let hand = []
-  // console.log("before: ", workingLetterPool)
+
   for (let i = 0; i < 10; i++){
     const drawnLetter = workingLetterPool[Math.floor(Math.random()*workingLetterPool.length)]
-    // console.log(drawnLetter)
-    // console.log("stopped")
     hand.push(drawnLetter)
-    // console.log(hand)
     const drawnLetterIndex = workingLetterPool.indexOf(drawnLetter)
-    // console.log(drawnLetterIndex)
     workingLetterPool.splice(drawnLetterIndex,1)
   }
-  // console.log("after: ", workingLetterPool)
-  // console.log("10x")
   return hand
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
   let workingLettersInHand = JSON.parse(JSON.stringify(lettersInHand))
   input = input.toUpperCase() 
   
   for (let i = 0; i < input.length; i++) {
-    // console.log("input[i]: ", input[i])
-    // console.log("i: ", i)
     if (workingLettersInHand.includes(input[i]) === true) {
-      // console.log("initial letters: ", workingLettersInHand)
       let letterIndex = workingLettersInHand.indexOf(input[i])
       workingLettersInHand.splice(letterIndex,1)
-      // console.log("remaining letters: ", workingLettersInHand)
     } else if (workingLettersInHand.includes(input[i]) === false) {
         return false
     }
@@ -47,10 +35,8 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
   let score = 0
   word = word.toUpperCase()
-  // console.log("word: " ,word)
 
   const letterScore = {
       A: 1,
@@ -81,16 +67,9 @@ export const scoreWord = (word) => {
       Z: 10,
       "":0
     }
-  // console.log("word length: ", word.length)
-  // if (word.length == 0) {
-  //   console.log(score)
-  //   return score;
-  // } else 
+
   for (let letter of word) {
-      // console.log(letter)
       score += letterScore[letter]
-      // console.log(score)
-      // score += letterScore.keys(letterScore).find(key => letterScore[key] === letter)
   }
   
   if (word.length >= 7) {
@@ -100,7 +79,6 @@ export const scoreWord = (word) => {
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
   let bestWord = ""
   let highestScore = 0
 
@@ -110,17 +88,10 @@ export const highestScoreFrom = (words) => {
     if (tempScore > highestScore) {
       highestScore = tempScore
       bestWord = word
-      console.log(word, "replaced word based on score")
 
     } else if (tempScore === highestScore) {
-      console.log("word: ", word, word.length)
-      console.log("best word: ",bestWord, bestWord.length)
       if(bestWord.length != 10 && (word.length === 10 || word.length < bestWord.length)){
         bestWord = word
-        // console.log(word, "replaced word because its length is 10")
-      // } else if (word.length < bestWord.length && bestWord.length !== 10) {
-      //   bestWord = word
-      //   console.log(word, "replaced word because it was shorter")
       }
     }
 
